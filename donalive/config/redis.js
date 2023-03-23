41d238a8fde17607cdaa8a5757a685303b0ce04a
coder <jokhendra@cynayd.com>
2023-03-23 16:11:50 +0530

    todo

--- donalive/config/redis.js
+++ donalive/config/redis.js
@@ -1,17 +1,17 @@
-// var {createClient}=require("redis");
+var {createClient}=require("redis");
 
-// var client;
-// async function redisConnection(){
-//   try {
-//    client=createClient();
-//    await client.connect()
-//    client.on("connect",()=>{
-//     console.log("redis server connected successfully!!");
-//    })
-//   } catch (error) {
-//     console.error("something is worng in redis server",error);
-//   }
-// }
+var client;
+async function redisConnection(){
+  try {
+   client=createClient();
+   await client.connect()
+   client.on("connect",()=>{
+    console.log("redis server connected successfully!!");
+   })
+  } catch (error) {
+    console.error("something is worng in redis server",error);
+  }
+}
 
-// redisConnection();
-// module.exports=client
+redisConnection();
+module.exports=client
